ae38d9a616a6fc28b52c78a4708f6a8504be2fd0
Jack Brewer <jack@jackbrewer.co.uk>
2021-09-21 13:07:59 +0100

    feat(ViewerCarousel): hide filmstrip if only one thumbnail

--- src/component/Primary/AutoWorksPlayer/AutoWorksPlayer.stories.jsx
+++ src/component/Primary/AutoWorksPlayer/AutoWorksPlayer.stories.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import AutoWorksPlayer from '.'
 
+import config from '@/fixture/config'
+import items from '@/fixture/items'
+
 export default {
   title: 'Unsorted/AutoWorksPlayer',
   component: AutoWorksPlayer
@@ -9,5 +12,13 @@ export default {
 const Template = (args) => <AutoWorksPlayer {...args} />
 
 export const Default = Template.bind({})
+Default.args = {
+  items,
+  ...config
+}
 
-Default.args = {}
+export const SingleItem = Template.bind({})
+SingleItem.args = {
+  items: [items[3]],
+  ...config
+}
