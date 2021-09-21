05a07a1d7c8cc0dd7d7ca6ba45a9a28f23b2692e
Jack Brewer <jack@jackbrewer.co.uk>
2021-09-21 15:57:40 +0100

    remove obsolete tests

--- src/component/Primitive/Zoomer/Zoomer.test.jsx
+++
@@ -1,8 +0,0 @@
-import validateRequiredProps from '@/lib/validate-required-props'
-import Zoomer from '.'
-
-const requiredProps = () => ({})
-
-describe('Component: Zoomer', function () {
-  validateRequiredProps(Zoomer, requiredProps())
-})
